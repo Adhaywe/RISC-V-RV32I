3fa4ff3615dc8d860266e8328cfa7b7b9ace846b
Adam <adamghebrezgi@gmail.com>
2025-07-02 12:14:54 +0200

    register file

--- Pipelined/data path/regfile.sv
+++ Pipelined/data path/regfile.sv
@@ -7,11 +7,11 @@
 
 
 
-module regfile (input logic         clk, 
-                input logic         we3,
-                input logic [4:0]   a1, a2, a3,
-                input logic [31:0]  wd3,
-                output logic [31:0] rd1, rd2
+module regfile (input  logic         clk, 
+                input  logic         we3,
+                input  logic [ 4:0]  a1, a2, a3,
+                input  logic [31:0]  wd3,
+                output logic [31:0]  rd1, rd2
 );
 
 logic [31:0] rf [31:0];

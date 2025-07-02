2bfedbb22311e991906f96e81be4cb76e4bbf745
Adam <adamghebrezgi@gmail.com>
2025-07-02 12:14:36 +0200

    mux

--- Pipelined/data path/mux3.sv
+++ Pipelined/data path/mux3.sv
@@ -9,10 +9,10 @@
 module mux3 #(
     parameter WIDTH = 32
 )
-            (
-    input logic [WIDTH-1:0]  d0, d1, d2,
-    input logic [1:0]        s,
-    output logic [WIDTH-1:0] y
+(
+    input  logic [WIDTH-1:0]  d0, d1, d2,
+    input  logic [      1:0]  s,
+    output logic [WIDTH-1:0]  y
 );
 
 

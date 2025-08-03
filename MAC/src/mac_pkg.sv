fbfe8cb5457d253509e02e5833828129688412ac
Adam <adamghebrezgi@gmail.com>
2025-08-03 18:08:12 +0200

    mac package

---
+++ MAC/src/mac_pkg.sv
@@ -0,0 +1,11 @@
+package mac_pkg;
+    localparam DATA_WIDTH = 72;
+    localparam MAT_SIZE   = 3;
+    localparam VAR_WIDTH  = 8;
+
+    localparam ADD = 2'b00;
+    localparam SUB = 2'b01;
+    localparam MUL = 2'b10;
+    localparam MMUL = 2'b11;
+
+endpackage

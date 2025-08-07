539c684c0d2f4b8fc191e492631dc85d856eda0a
Adam <adamghebrezgi@gmail.com>
2025-08-08 00:21:53 +0200

    package file

--- Pipelined/MAC/src/mac_pkg.sv
+++ Pipelined/MAC/src/mac_pkg.sv
@@ -1,11 +1,10 @@
 package mac_pkg;
+
     localparam DATA_WIDTH = 72;
     localparam MAT_SIZE   = 3;
     localparam VAR_WIDTH  = 8;
-
-    localparam ADD = 2'b00;
-    localparam SUB = 2'b01;
-    localparam MUL = 2'b10;
-    localparam MMUL = 2'b11;
+    localparam MADD = 2'b00;
+    localparam MSUB = 2'b01;
+    localparam MMUL = 2'b10;
 
 endpackage

a26cb3aaa578eda73d1de8510a117c159e5fffa0
Adam <adamghebrezgi@gmail.com>
2025-08-08 15:15:07 +0200

    data memory

--- Pipelined/data path/dmem.sv
+++ Pipelined/data path/dmem.sv
@@ -1,23 +1,30 @@
 //******************************************************************************************
 // Design: dmem.sv
-// Author: Adam 
+// Author: Adam
 // Description: data memory
 // v 0.1
 //******************************************************************************************
 
 
 
-module dmem (input  logic         clk, we, 
+module dmem (input  logic         clk, we,
              input  logic [31:0]  a, wd,
              output logic [31:0]  rd
             );
 
-logic [31:0] RAM [63:0];
+logic [31:0] RAM [127:0]; // changed from 63 to 127
 
 assign rd = RAM[a[31:2]];
 
 always_ff @(posedge clk)
     if (we)
       RAM[a[31:2]] <= wd;
+
+
+
+initial begin
+  $readmemh("C:/Users/Adam/Downloads/PortableGit/RISC-V-RV32I/Pipelined/data path/dmem.hex", RAM);
+end
+
 
 endmodule
